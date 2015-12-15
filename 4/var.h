934ecbd5d5aa4b547d7b9da4d1c016fb3972c64f
emmanuelxavier040 <emmanuelxavier040@gmail.com>
2015-12-15 13:23:08 +0530

    4

--- 4/var.h
+++ 4/var.h
@@ -13,3 +13,17 @@ struct vnode{
 
 #define YYSTYPE struct vnode*
 
+
+
+
+struct Tnode {
+		int TYPE;
+		int NODETYPE;
+		char* NAME; 
+		int VALUE; 
+		Tnode *ArgList;
+		Tnode *Ptr1, *Ptr2, *Ptr3;
+		Gsymbol *Gentry; 
+		Lsymbol *Lentry; 
+};
+struct Tnode *TreeCreate(TYPE,NODETYPE,VALUE,NAME,ArgList,Ptr1, Ptr2, Ptr3);
